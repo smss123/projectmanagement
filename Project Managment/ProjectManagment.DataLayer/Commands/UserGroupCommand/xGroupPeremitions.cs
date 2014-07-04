8c4456487cb40e998f2a482890375d588eaee798
smss123 <smsssamer@live.com>
2014-07-04 03:57:18 -0700

    bound classes

---
+++ Project Managment/ProjectManagment.DataLayer/Commands/UserGroupCommand/xGroupPeremitions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagment.DataLayer.Commands.UserGroupCommand
+{
+   public  class xGroupPeremitions
+    {
+
+       private ProjectDbEntities db = new ProjectDbEntities();
+       public bool AddPeremition(UserGroupPermession Per)
+       {
+
+           try
+           {
+               db.UserGroupPermessions.Add(Per);
+               db.SaveChanges();
+               return true;
+           }
+           
+           catch (Exception)
+           {
+
+               return false;
+           }
+       }
+
+       public bool EditGroupPeremitions(UserGroupPermession UsrPer)
+       {
+           try
+           {
+                  var p = db.UserGroupPermessions.Where(x => x.Id == UsrPer.Id).SingleOrDefault();
+                   if (p.Id != null)
+                   {
+                       p.ValueX = UsrPer.ValueX;
+                       p.Permession = UsrPer.Permession;
+                       p.UserGroup = UsrPer.UserGroup;
+                       db.SaveChanges();
+                       return true;
+                   }
+                   else { return false; }
+                   }
+           catch (Exception)
+           {
+
+               return false;
+           }
+
+       }
+       public bool DeletePeremtion(UserGroupPermession UsrPer)
+       {
+
+           try
+           {
+               var p = db.UserGroupPermessions.Where(x => x.Id == UsrPer.Id).SingleOrDefault();
+               if (p.Id != null)
+               {
+                   db.UserGroupPermessions.Remove(UsrPer);
+                   db.SaveChanges();
+                   return true;
+               }
+               else { return false; }
+           }
+           catch (Exception)
+           {
+
+               return false;
+           }
+       }
+
+           public List <UserGroupPermession> GetAllPeremitions(){
+               var AllPeremitions = ( from per in db .UserGroupPermessions
+                                      orderby per .Id ascending
+                                      select per ).ToList () ;
+               return AllPeremitions ;
+
+           }
+       }
+
+
+    }
+}
---
+++ Project Managment/ProjectManagment.DataLayer/Commands/UserGroupCommand/xProjectProfile.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagment.DataLayer.Commands.UserGroupCommand
+{
+   public  class xProjectProfile
+    {
+        private ProjectDbEntities db = new ProjectDbEntities();
+
+
+       public bool AddNewProject( ProjectProfile ptb){
+           try 
+	        {
+                db.ProjectProfiles.Add(ptb); db.SaveChanges();
+		    return true ;
+	        }
+	        catch (Exception)
+	        {
+		
+		       return false ;
+	        }
+       }
+
+
+       public bool EditProject(ProjectProfile ptb)
+       {
+           try
+           {
+               var tb = db.ProjectProfiles.Where(p => p.ID == ptb.ID).SingleOrDefault();
+               if (tb.ID != null)
+               {
+                   tb.ProjectName = ptb.ProjectName;
+                   tb.ProjectDescription = ptb.ProjectDescription;
+                   tb.StartDate = ptb.StartDate;
+                   tb.EndDate = ptb.EndDate;
+                   tb.Status = ptb.Status;
+                   tb.progress = ptb.progress;
+                   tb.TotalCost = ptb.TotalCost;
+                   db.SaveChanges();
+                   return  true ;
+               }else { return false ;}
+           }
+           catch (Exception)
+           {
+
+               return false;
+           }
+       }
+
+       public bool DeleteProject(ProjectProfile ptb)
+       {
+           try
+           {
+               var tb = db.ProjectProfiles.Where(p => p.ID == ptb.ID).SingleOrDefault();
+               if (tb.ID != null)
+               {
+                   db.ProjectProfiles.Remove(ptb);
+                   db.SaveChanges();
+                   return true;
+               }
+               else { return false; }
+           }
+           catch (Exception)
+           {
+
+               return false;
+           }
+
+       }
+
+
+
+
+        public List<ProjectProfile> GetAllProjects() { return (db.ProjectProfiles).ToList(); }
+
+
+    }
+}
